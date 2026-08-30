7ec62ab92943de1cfb9859284be11897fca3194c
Abdo Othman <abdoetman010@gmail.com>
2026-08-30 16:08:05 +0300

    [ADD] Implement CurrentUser service to get info from the current user

---
+++ src/ProjectManager.API/Services/CurrentUserService.cs
@@ -0,0 +1,31 @@
+using System.Security.Claims;
+using Application.Common.Interfaces;
+
+namespace ProjectManager.API.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid? UserId
+    {
+        get
+        {
+            var userIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("sub");
+
+            return Guid.TryParse(userIdClaim, out var userId) ? userId : null;
+        }
+    }
+
+    public string? Email =>
+        _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+
+    public bool IsAuthenticated =>
+        _httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+}
